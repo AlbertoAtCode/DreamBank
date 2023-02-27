9107f1042f6e1b055888822d2706886608b3581c
AlbertoAtCode <alberto.aniballi@mail.polimi.it>
2023-02-27 20:44:39 +0100

    Creating the first Stages

--- src/main/java/com/example/dreambank/Views/ViewFactory.java
+++ src/main/java/com/example/dreambank/Views/ViewFactory.java
@@ -1,4 +1,54 @@
 package com.example.dreambank.Views;
 
+import com.example.dreambank.Controllers.Client.ClientController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 public class ViewFactory {
+    // Client views
+    private AnchorPane dashboardView;
+
+    public ViewFactory() {}
+
+    public AnchorPane getDashboardView() {
+        if (dashboardView==null) { //if the object has already been created we don't want to reload it, we load it only once if it is null
+            try {
+                dashboardView = new FXMLLoader(getClass().getResource("/Fxml/Client/Dashboard.fxml")).load();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return dashboardView;
+    }
+
+    public void showLoginWindow() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
+        createStage(loader);
+    }
+
+    public void showClientWindow() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Client/Client.fxml"));
+        ClientController clientController = new ClientController();
+        loader.setController(clientController);
+        createStage(loader);
+    }
+
+    private void createStage(FXMLLoader loader) {
+        Scene scene = null;
+        try {
+            scene = new Scene(loader.load());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Dream Bank");
+        stage.show();
+    }
+
+    public void closeStage(Stage stage) {
+        stage.close();
+    }
 }
